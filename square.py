a049bcd5f6e483dd9746e3c2b678fdbcbf9fc626
Oldspider1488 <58492494+Oldspider1488@users.noreply.github.com>
2019-12-14 17:17:31 +0300

    Update square.py

--- square.py
+++ square.py
@@ -1,7 +1,4 @@
-first_side = int(input('1-st side: '))
-second_side = int(input('2-nd side: '))
-slab = int(input('slab: '))
-first_side = first_side % slab
-second_side = second_side % slab
-tmp = first_side * second_side
-print(tmp)
+a, b, c = map(int, input().split())
+a = (a + c - 1) // c
+b = (b + c - 1) // c
+print(a * b)
