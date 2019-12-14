93a33acaa5e7e6fe7074511e23685aa457f2002c
Oldspider1488 <58492494+Oldspider1488@users.noreply.github.com>
2019-12-14 16:06:31 +0300

    Update watermelon.py

--- watermelon.py
+++ watermelon.py
@@ -3,7 +3,9 @@ try:
 except ValueError:
     print('invalid value')
 else:
-    if watermelon % 2 == 0:
-        print('yes')
+    if watermelon == 2:
+        print("NO")
+    elif watermelon % 2 == 0:
+        print("YES")
     else:
-        print('no')
+        print("NO")
